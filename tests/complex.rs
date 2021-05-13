272b232a83b5a2fda1c2d0e2797c9d876ea4b022
Sasha <sasha@thisminute.org>
2021-05-13 05:50:59 -0400

    add passing compilation test

--- tests/complex.rs
+++ tests/complex.rs
@@ -9,25 +9,52 @@ wasm_bindgen_test_configure!(run_in_browser);
 cwl_header!();
 
 #[wasm_bindgen_test]
-fn element() {
+fn dynamic() {
 	cwl_test_setup! {
-		text: "hi";
-		.bongo {
+		text: "click me";
+		.a {
 			?click {
-				text: "click the text under this";
-				sdaf {
-					text: "nope";
+				text: "I've been clicked!";
+				b {
+					text: "hello world";
 				}
 			}
 			color: "blue";
-			text: "yeaaaa";
+			text: "click me too";
 		}
 		?click {
-			// .sdaf {
-			// 	color: "green";
-			// 	text: "yep";
-			// }
-			bongo {}
+			a {}
+			.b {
+				color: "green";
+				text: "nope";
+			}
 		}
 	}
+	// assert_eq!(
+	// 	root.first_child()
+	// 		.expect("the root should contain a node")
+	// 		.text_content()
+	// 		.expect("the node should contain text"),
+	// 	"click me"
+	// );
+	// root.click();
+	// assert_eq!(
+	// 	root.first_element_child()
+	// 		.expect("the root should now contain an element")
+	// 		.inner_html(),
+	// 	"click me too"
+	// );
+	// root.first_element_child()
+	// 	.expect("the root should now contain an element")
+	// 	.dyn_into::<HtmlElement>()
+	// 	.expect("this cast should work")
+	// 	.click();
+	// assert_eq!(
+	// 	root.first_element_child()
+	// 		.expect("the root should still contain an element")
+	// 		.first_element_child()
+	// 		.expect("that element should now contain an element")
+	// 		.inner_html(),
+	// 	"hello world"
+	// );
 }
