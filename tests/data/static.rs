98c718e2218a2cfadf085db34558540c2701ee26
Sasha Kondrashov <timofey.kondrashov@vroom.com>
2021-11-25 08:15:20 -0500

    add static class tests for variables

--- tests/data/static.rs
+++ tests/data/static.rs
@@ -16,3 +16,41 @@ fn base() {
 	}
 	assert_eq!(root.inner_html(), "hello world");
 }
+
+#[wasm_bindgen_test]
+fn class_1() {
+	test_setup! {
+		a {
+			text: $text;
+		}
+		.a {
+			$text: "hello world";
+		}
+	}
+	assert_eq!(
+		root
+			.first_element_child()
+			.expect("the root should contain an element")
+			.inner_html(),
+		"hello world"
+	);
+}
+
+#[wasm_bindgen_test]
+fn class_2() {
+	test_setup! {
+		a {
+			$text: "hello world";
+		}
+		.a {
+			text: $text;
+		}
+	}
+	assert_eq!(
+		root
+			.first_element_child()
+			.expect("the root should contain an element")
+			.inner_html(),
+		"hello world"
+	);
+}
